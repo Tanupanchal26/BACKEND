3dfd7c910ebe5307cf19ca03142df87a72eaac53
tanya <tanyapanchal65@gamil.com>
2025-01-04 21:05:13 +0530

    done the CURD op

--- server.js
+++ server.js
@@ -1,8 +1,49 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const Model = require('./Model/UserSchema')
-const connectDb = require('./Config/db')
-connectDb()
+const connectDb = require("./config/db");
+const userModel = require("./model/userSchema");
+
+connectDb();
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+app.get("/", (req, res) => {
+  res.send("HEllO");
+});
+
+app.post("/registergit", async (req, res) => {
+  const { email, password, name } = req.body;
+  const userExist = await userModel.findOne({ email });
+  if (userExist) {
+    res.send({ message: "User Exist" });
+  }
+  const newUser = new userModel({ name, email, password });
+  await newUser.save();
+  res.send({ message: "User Created Successfully" });
+});
+
+app.delete('/delete/:id',async(req,res)=>{
+  const id=req.params.id;
+  const userDelete=await userModel.findOneAndDelete(id)
+  if(userDelete){
+    res.send({message:"User Deleted Successfully"})
+  } else{
+  res.send({message:"User not exist"})
+  }
+})
+
+app.put('/update/:id',async (req,res)=>{
+  const itemId=req.params.id
+  const updatedId=req.body
+  const userUpdate=await userModel.findByIdAndUpdate({_id:itemId},updatedId,{new:true})
+  if(userUpdate){
+    res.send({message:"User Updated Successfully"})
+  }
+  else{
+    res.send({message:"User not updated Successfully"})
+  }
+})
+
 app.listen(3000, () => {
-    console.log("Heloooo")
+  console.log("Server is running... ");
 });
